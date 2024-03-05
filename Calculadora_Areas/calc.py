60e2b3c35d1c553a142ebdea327b1c69e5d96167
Marcelo Porfirio <79551082+MarceloPorfirio@users.noreply.github.com>
2024-03-05 06:45:39 -0300

    add

---
+++ Calculadora_Areas/calc.py
@@ -0,0 +1,54 @@
+import flet as ft
+
+
+def main(page:ft.Page):
+    
+    page.window_maximized = True
+    page.vertical_alignment = ft.MainAxisAlignment.CENTER
+    page.horizontal_alignment = ft.CrossAxisAlignment.CENTER
+    
+    def calc_area(e):
+        ...
+        
+    title = ft.Text('Cálculo Facilitado para construção',size=30)
+    area_container = ft.Container(
+        ft.Row([
+            ft.Column(
+            controls=[
+                area1 := ft.TextField(label='Area 1'),
+                area2 := ft.TextField(label='Area 2'),
+                ft.ElevatedButton('Calcular',on_click=calc_area)
+            ],
+            
+        ),
+            ft.Container(
+                width=100,
+                height=100,
+                margin=ft.margin.only(bottom=30),
+                border_radius=ft.border_radius.all(30),
+                alignment=ft.alignment.center,
+                bgcolor=ft.colors.RED,
+                content=ft.Text('Resultado')
+            )
+        ]),
+        
+        
+        
+        
+        
+    )
+    
+    main_container = ft.Container(
+        bgcolor=ft.colors.CYAN,
+        expand=True,
+        content=ft.Column(
+            controls=[
+                
+                area_container,
+            ]
+        )
+    )
+    
+    page.add(title,main_container)
+
+ft.app(target=main)
