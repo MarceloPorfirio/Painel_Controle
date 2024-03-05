f76a12f4ec23b11cc90f17cd7c7ad7c7988e5836
Marcelo Porfirio <79551082+MarceloPorfirio@users.noreply.github.com>
2024-03-05 12:48:18 -0300

    Update calc.py

--- Calculadora_Areas/calc.py
+++ Calculadora_Areas/calc.py
@@ -12,8 +12,15 @@ def main(page:ft.Page):
         
     title = ft.Text('Cálculo Facilitado para construção',size=30)
     area_container = ft.Container(
-        ft.Row([
+        width=500,
+        border=ft.border.all(width=2),
+        border_radius=ft.border_radius.all(10),
+        padding=10,
+        content= ft.Row(
+        
+            [
             ft.Column(
+            
             controls=[
                 area1 := ft.TextField(label='Area 1'),
                 area2 := ft.TextField(label='Area 2'),
@@ -24,10 +31,11 @@ def main(page:ft.Page):
             ft.Container(
                 width=100,
                 height=100,
-                margin=ft.margin.only(bottom=30),
-                border_radius=ft.border_radius.all(30),
+                margin=ft.margin.only(bottom=40,left=40),
+                border_radius=ft.border_radius.all(10),
+                border=ft.border.all(width=2),
                 alignment=ft.alignment.center,
-                bgcolor=ft.colors.RED,
+                # bgcolor=ft.colors.RED,
                 content=ft.Text('Resultado')
             )
         ]),
